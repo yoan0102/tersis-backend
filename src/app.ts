2e089ee201e3d99468349cd69a45dcc9c5d7f87f
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2023-10-07 19:05:24 -0400

    feat: add cors credetnials

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express, { Application, Response } from 'express'
-console.log('dirname', __dirname)
 import cors from 'cors'
 import 'express-async-errors'
 import cookieParser from 'cookie-parser'
@@ -15,35 +14,28 @@ import config from './config'
 
 const app: Application = express()
 
-const WHITE_LIST = [config.origin, '*']
+const WHITE_LIST = [config.origin]
 
-// app.use(
-// 	cors({
-// 		origin: function (origin, cab) {
-// 			if (WHITE_LIST.includes(origin)) {
-// 				return cab(null, origin)
-// 			}
+app.use(
+	cors({
+		origin: function (origin, cab) {
+			if (WHITE_LIST.includes(origin)) {
+				return cab(null, origin)
+			}
 
-// 			return cab(
-// 				new Error('Error de CORS origin: ' + origin + 'not authtorization')
-// 			)
-// 		},
-// 		credentials: true,
-// 	})
-// )
+			return cab(
+				new Error('Error de CORS origin: ' + origin + 'not authtorization')
+			)
+		},
+		credentials: true,
+	})
+)
 
-// app.use(cors())
-
-// app.use(pino())
 app.use(cookieParser())
 app.use(helmet())
 app.use(compression())
 app.use(express.json())
-app.use(express.static(__dirname + 'uploads'))
-app.get('/cookie', (req, res: Response) => {
-	console.log('Llego la peticion')
-	res.cookie('12', '12').send('cookie')
-})
+app.use('/uploads', express.static('uploads'))
 
 app.use('/api/v1/tracks', new TrackRoutes().getRoutes())
 app.use('/api/v1/users', new UserRoutes().getRoutes())
