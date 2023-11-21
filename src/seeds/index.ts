1ea864ee75593347b53951fd7bfdc9b60fec5dd0
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2023-11-21 15:49:34 -0500

    fix:fixed seed

--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -1,3 +1,1 @@
-import createSuperUser from './seedSuperUser'
-
-createSuperUser()
+import './seedSuperUser'
--- src/seeds/seedSuperUser.ts
+++ src/seeds/seedSuperUser.ts
@@ -1,7 +1,10 @@
+import mongoose from 'mongoose'
+import config from '../config'
 import { UserModel } from '../api/users/models/user.schema'
 import { UserDTORegister } from '../api/users/dto'
 
 async function createSuperUser() {
+	await mongoose.connect(config.mongoUri || '')
 	const userDto = new UserDTORegister({
 		name: 'Admin',
 		email: 'admin@admin.com',
@@ -9,7 +12,17 @@ async function createSuperUser() {
 		favorites: [],
 		role: 'Admin',
 	})
-	await UserModel.create(userDto)
+	UserModel.create(userDto)
+		.then(() => {
+			console.log('Admin creted')
+		})
+		.catch((error) => {
+			console.log(error)
+			console.log('Seed executed')
+		})
+		.finally(() => {
+			mongoose.connection.close()
+		})
 }
 
-export default createSuperUser
+createSuperUser()
