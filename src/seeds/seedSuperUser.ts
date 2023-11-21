6b845b08ecbd875e8fcd27739ba70850f30275d5
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2023-11-21 14:35:02 -0500

    fix : fixed seed

--- src/seeds/seedSuperUser.ts
+++ src/seeds/seedSuperUser.ts
@@ -1,15 +1,14 @@
 import { UserModel } from '../api/users/models/user.schema'
 import { UserDTORegister } from '../api/users/dto'
 
-const userDto = new UserDTORegister({
-	name: 'Admin',
-	email: 'admin@admin.com',
-	password: 'admin',
-	favorites: [],
-	role: 'Admin',
-})
-
 async function createSuperUser() {
+	const userDto = new UserDTORegister({
+		name: 'Admin',
+		email: 'admin@admin.com',
+		password: 'admin',
+		favorites: [],
+		role: 'Admin',
+	})
 	await UserModel.create(userDto)
 }
 
